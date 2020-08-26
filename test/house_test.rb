f21fa3390a27fb52626d7fd66934b8cc72d6e893
George Soderholm <georgesoderholm@gmail.com>
2020-08-26 10:07:21 -0600

    Add to check area, categorize, convert to hash

--- test/house_test.rb
+++ test/house_test.rb
@@ -53,4 +53,52 @@ class HouseTest < Minitest::Test
     assert_equal [room_1, room_2], house.rooms
   end
 
+  def test_it_can_test_if_the_house_is_above_average
+    # skip
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal false, house.above_market_average?
+  end
+
+  def test_it_can_group_rooms_by_their_category
+    # skip
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    house.rooms_from_category(:bedroom)
+
+    assert_equal [room_1, room_2] house.rooms_from_category(:bedroom)
+  end
+
+  def test_it_can_figure_out_the_total_area_of_rooms_in_a_house
+    # skip
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal 1900, house.area
+  end
+  def test_it_can_show_the_details_of_the_house
+    # skip
+    house = House.new("$400000", "123 sugar lane")
+
+    answer = {"price" => 400000, "address" => "123 sugar lane"}
+    assert_equal answer, house.details
+  end
+
+
 end
